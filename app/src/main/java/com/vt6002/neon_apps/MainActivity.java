939ca4cf8a6c990904c49034587b93e290d0f42d
itcp-ly <227106050@stu.vtc.edu.hk>
2023-01-09 13:30:34 +0800

    add file

---
+++ app/src/main/java/com/vt6002/neon_apps/MainActivity.java
@@ -0,0 +1,121 @@
+package com.vt6002.neon_apps;
+
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.media.Image;
+import android.opengl.GLES30;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.ar.core.Anchor;
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Camera;
+import com.google.ar.core.Config;
+import com.google.ar.core.Config.InstantPlacementMode;
+import com.google.ar.core.DepthPoint;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.InstantPlacementPoint;
+import com.google.ar.core.LightEstimate;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Point;
+import com.google.ar.core.Point.OrientationMode;
+import com.google.ar.core.PointCloud;
+import com.google.ar.core.Session;
+import com.google.ar.core.Trackable;
+import com.google.ar.core.TrackingFailureReason;
+import com.google.ar.core.TrackingState;
+import com.vt6002.neon_apps.common.helpers.CameraPermissionHelper;
+import com.vt6002.neon_apps.common.helpers.DepthSettings;
+import com.vt6002.neon_apps.common.helpers.DisplayRotationHelper;
+import com.vt6002.neon_apps.common.helpers.FullScreenHelper;
+import com.vt6002.neon_apps.common.helpers.InstantPlacementSettings;
+import com.vt6002.neon_apps.common.helpers.SnackbarHelper;
+import com.vt6002.neon_apps.common.helpers.TapHelper;
+import com.vt6002.neon_apps.common.helpers.TrackingStateHelper;
+import com.vt6002.neon_apps.common.samplerender.Framebuffer;
+import com.vt6002.neon_apps.common.samplerender.GLError;
+import com.vt6002.neon_apps.common.samplerender.Mesh;
+import com.vt6002.neon_apps.common.samplerender.SampleRender;
+import com.vt6002.neon_apps.common.samplerender.Shader;
+import com.vt6002.neon_apps.common.samplerender.Texture;
+import com.vt6002.neon_apps.common.samplerender.VertexBuffer;
+import com.vt6002.neon_apps.common.samplerender.arcore.BackgroundRenderer;
+import com.vt6002.neon_apps.common.samplerender.arcore.PlaneRenderer;
+import com.vt6002.neon_apps.common.samplerender.arcore.SpecularCubemapFilter;
+import com.google.ar.core.exceptions.CameraNotAvailableException;
+import com.google.ar.core.exceptions.NotYetAvailableException;
+import com.google.ar.core.exceptions.UnavailableApkTooOldException;
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
+import com.vt6002.neon_apps.databinding.ActivityMainBinding;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
